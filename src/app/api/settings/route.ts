a29f5a375c9043d3d368fefaa1af18b5a099b0fa
Rupesh Kumar Singh <121041587+rupeshksingh@users.noreply.github.com>
2024-08-21 16:16:14 +0530

    Update route.ts

--- src/app/api/settings/route.ts
+++ src/app/api/settings/route.ts
@@ -3,7 +3,7 @@ import { NextRequest, NextResponse } from "next/server";
 export async function GET(req: NextRequest): Promise<NextResponse> {
   const tokenLimit = process.env.VLLM_TOKEN_LIMIT
     ? parseInt(process.env.VLLM_TOKEN_LIMIT)
-    : 4096;
+    : 512;
 
   return NextResponse.json(
     {
